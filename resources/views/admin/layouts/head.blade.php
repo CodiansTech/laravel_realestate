a5ef4e8205e1802ab299655770eafa60b068ff92
rinoj <jeezybox@gmail.com>
2020-05-25 21:57:04 +0200

    Admin Layout

---
+++ resources/views/admin/layouts/head.blade.php
@@ -0,0 +1,17 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="keywords" content="advanced search custom, agency, agent, business, clean, corporate, directory, google maps, homes, listing, membership packages, property, real estate, real estate agent, realestate agency, realtor">
+<meta name="description" content="FindHouse - Real Estate HTML Template">
+<meta name="CreativeLayers" content="ATFN">
+<!-- css file -->
+<link rel="stylesheet" href="{{URL::asset('assets/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{URL::asset('assets/css/style.css')}}">
+<link rel="stylesheet" href="{{URL::asset('assets/css/dashbord_navitaion.css')}}">
+<!-- Responsive stylesheet -->
+<link rel="stylesheet" href="{{URL::asset('assets/css/responsive.css')}}">
+<!-- Title -->
+<title>FindHouse - Real Estate HTML Template</title>
+<!-- Favicon -->
+<link href="images/favicon.ico" sizes="128x128" rel="shortcut icon" type="image/x-icon" />
+<link href="images/favicon.ico" sizes="128x128" rel="shortcut icon" />
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.index');
 });
+
+
+Route::get('/admin', function () {
+    return view('admin.pages.index');
+});
