ccd28175b4171b7705d1f28331e31a1bad74bf28
rinoj <jeezybox@gmail.com>
2020-05-27 12:04:24 +0200

    navbar update

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages.index');
-});
+})->name('index');
 
 Route::get('house', function () {
     return view('pages.properties.index');
